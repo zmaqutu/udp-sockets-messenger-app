31c69dad93dbbe31799fe885721ad6901b9158f7
Zongo Maqutu <MQTZON001@myuct.ac.za>
2021-04-02 02:26:22 +0200

    Added Server method to receive a message

--- src/Server.java
+++ src/Server.java
@@ -7,7 +7,7 @@ import java.net.InetAddress;
 public class Server Extends JFrame{
 	private JTextField userInput;
 	private JTextArea chatWindow;
-	private DatagramSocket serverSocket = new DatagramSocket(9999);
+	private DatagramSocket serverSocket;
 
 	//Constructor
 	public Server(){
@@ -31,9 +31,32 @@ public class Server Extends JFrame{
 	//setup and run the server, this will be called after GUI is setup
 	public void startRunning(){
 		try{
+			serverSocket = new DatagramSocket(9999);
+			while(true){
+				try{
+					DatagramPacket packet = receiveMessage();
+				}catch(EOFException e){
+					showMessage("\n Server Ended the Connection");
+				}finally{
+					close();
+				}
+			}
 		}catch (IOExceotion e){
 			e.printStackTrace();
 		}
+	}
+	//this is a method that receives a message and stores it in a string
+	public DatagramPacket receiveMessage() throws IOException{
+		byte [] dataReceived = new byte[1024];		//the data will be received as an array of bytes
+		
+		DatagramPacket receivePacket = new DatagramPacket(dataReceived,dataReceived.length);
+                serverSocket.receive(receivePacket);
+
+		//String receivedMessage = String(receivePacket.getData());
+		//showMessage(receivedMessage1);
+		return receivePacket;
+
+
 	}
 	
 	
